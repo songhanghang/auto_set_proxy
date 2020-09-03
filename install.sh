2c171a49e0d0170fe0f8ec273b6b8063237448a7
SongHang <1131442853@qq.com>
2020-09-03 22:24:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
-yes | rm -rf $HOME/lib/easyproxy
-mkdir $HOME/lib/autoshell
-cp easy_proxy.sh $HOME/lib/easyproxy/
-bashrc_file="${HOME}/.bashrc"
-zshrc_file="${HOME}/.zshrc"
-auto_alias="alias easyproxy='sh $HOME/lib/autoshell/easy_proxy.sh'"
-echo "开始配置 bash auto_alias ..."
+targetPath=$HOME/lib/easyproxy
+sh_name="easyproxy"
+yes | rm -rf $targetPath
+mkdir $targetPath
+cp ./easy_proxy.sh $targetPath/
+bashrc_file=${HOME}/.bashrc
+zshrc_file=${HOME}/.zshrc
+auto_alias="alias $sh_name='sh $targetPath/easy_proxy.sh'"
 touch "$bashrc_file"
 if [[ -z $(grep "$auto_alias" "$bashrc_file") ]]; then
+    echo "配置 bash easyproxy alias ..."
     echo  >> $bashrc_file
     echo "$auto_alias" >> "$bashrc_file"
     source $bashrc_file
 fi
 
-echo "开始配置 zsh auto_alias ..."
 touch $zshrc_file
 if [[ -z $(grep "$auto_alias" "$zshrc_file") ]]; then
+    echo "配置 zsh easyproxy alias ..."
     echo  >> $zshrc_file
     echo "$auto_alias" >> "$zshrc_file"
     source $zshrc_file
 fi
 
-echo "安装完成..."
+echo "success"
 echo "开始愉快玩耍吧！ "
-echo "设置代理 '$auto_alias set'"
-echo "清除代理 '$auto_alias clean' "
+echo "~~~~~~~~~~~~~~~~~~~~~~~~ "
+echo "设置代理 \$$sh_name set"
+echo "清除代理 \$$sh_name clean"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~ "
 echo "注意！！！ 开启代理后WiFi高级选项看不到，必须使用命令清除代理 "
