918549d8ac1905dc730c9c5b130a9b47b0a44da9
SongHang <1131442853@qq.com>
2020-09-04 14:45:49 +0800

    Update easy_proxy.sh
    fix 可能获取不到IP

--- easy_proxy.sh
+++ easy_proxy.sh
@@ -6,7 +6,7 @@ echo "设置自定义代理   easyproxy set ****:8888"
 echo "删除代理        easyproxy clean"
 echo "--------------------------------------------\n"
 
-# 获取IP
+# 获取 IP
 ip=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 default_proxy=${ip}":8888"
 
