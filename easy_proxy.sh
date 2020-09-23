0729e19e330c512d6800245e5d02dc1c9a8d80ea
SongHang <1131442853@qq.com>
2020-09-23 14:52:28 +0800

    Update easy_proxy.sh

--- easy_proxy.sh
+++ easy_proxy.sh
@@ -6,22 +6,21 @@ echo "设置自定义代理   easyproxy set ****:8888"
 echo "删除代理        easyproxy clean"
 echo "--------------------------------------------\n"
 
-# 获取 IP
-ip=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
-echo $ip
-count=$(echo $ip | tr ' ' '\n' | wc -l )
-if [ $count -gt 1 ];then
-    echo "多个ip,请手动选择一个"
-    exit
-fi
-default_proxy=${ip}":8888"
-
-echo "本机IP为: $default_proxy"
 if [ "$1" == "set" ];then
     if [ -n "$2" ];then
         echo "设置自定义代理 $2"
         adb shell settings put global http_proxy $2
     else
+        # 获取 IP
+        ip=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+        echo $ip
+        count=$(echo $ip | tr ' ' '\n' | wc -l )
+        if [ $count -gt 1 ];then
+        echo "多个ip,请手动选择一个"
+        exit
+        fi
+        default_proxy=${ip}":8888"
+        echo "本机IP为: $default_proxy"
         echo "设置本机IP代理 $default_proxy"
         adb shell settings put global http_proxy $default_proxy
     fi
